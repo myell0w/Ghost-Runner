82141ddeb3afbe3b0ac00197072cdd736be2f46c
Gerhard Schraml <gerhard@mac-gspot.local>
2010-04-08 12:56:09 +0200

    g-spot arrived at the project

--- src/at/ac/tuwien/hci/ghost/StartRunActivity.java
+++ src/at/ac/tuwien/hci/ghost/StartRunActivity.java
@@ -1,13 +1,38 @@
 package at.ac.tuwien.hci.ghost;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 
 public class StartRunActivity extends Activity {
+	private static final int DIALOG_BLOED = 101;
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        showDialog(DIALOG_BLOED);
+    }
+    
+    protected Dialog onCreateDialog(int id) {
+        Dialog dialog = null;
+        AlertDialog.Builder builder = null;
+        switch(id) {
+        case DIALOG_BLOED:
+            builder = new AlertDialog.Builder(this);
+            builder.setMessage("Wer das liest ist blšd!");
+            builder.setNeutralButton("Eh kloa!", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int id) {
+                    dialog.cancel();
+               }
+            });
+            dialog = builder.create();
+            break;
+        default:
+        	dialog = null;
+        }
+        return dialog;
     }
 }
