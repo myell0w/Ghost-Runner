c980cd383986b4d439c9ad263237f586fd34529e
Gerhard Schraml <gerhard@mac-gspot.lan>
2010-04-19 15:03:42 +0200

    keine ahnung was ich gemacht habe

--- src/at/ac/tuwien/hci/ghost/data/dao/DataAccessObject.java
+++ src/at/ac/tuwien/hci/ghost/data/dao/DataAccessObject.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
-import android.util.Log;
 import at.ac.tuwien.hci.ghost.data.entities.Entity;
 
 public abstract class DataAccessObject {
