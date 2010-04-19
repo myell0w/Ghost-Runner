abba1f79bf5c3eabd51c620b7e255e76f9ab0bac
Matthias <matthias.tretter@gmail.com>
2010-04-19 12:57:44 +0200

    began RunningInfoActivity

---
+++ src/at/ac/tuwien/hci/ghost/ui/run/RunningMapActivity.java
@@ -0,0 +1,28 @@
+package at.ac.tuwien.hci.ghost.ui.run;
+
+import android.os.Bundle;
+import at.ac.tuwien.hci.ghost.R;
+
+import com.google.android.maps.MapActivity;
+import com.google.android.maps.MapView;
+
+public class RunningMapActivity extends MapActivity {
+	private MapView mapView = null;
+	
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.startrun);
+
+		//mapView = (MapView) findViewById(R.id.overviewMap);
+		//mapView.setBuiltInZoomControls(true);
+	}
+
+	@Override
+	protected boolean isRouteDisplayed() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
--- src/at/ac/tuwien/hci/ghost/ui/run/StartRunActivity.java
+++ src/at/ac/tuwien/hci/ghost/ui/run/StartRunActivity.java
@@ -1,6 +1,10 @@
 package at.ac.tuwien.hci.ghost.ui.run;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 import at.ac.tuwien.hci.ghost.R;
 
 import com.google.android.maps.MapActivity;
@@ -8,6 +12,7 @@ import com.google.android.maps.MapView;
 
 public class StartRunActivity extends MapActivity {
 	private MapView mapView = null;
+	private Button startButton = null;
 	
 	/** Called when the activity is first created. */
 	@Override
@@ -15,8 +20,18 @@ public class StartRunActivity extends MapActivity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.startrun);
 
-		//mapView = (MapView) findViewById(R.id.overviewMap);
-		//mapView.setBuiltInZoomControls(true);
+		mapView = (MapView) findViewById(R.id.overviewMap);
+		mapView.setBuiltInZoomControls(true);
+		
+		startButton = (Button)findViewById(R.id.startRunButton);
+		startButton.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				Intent runningInfoIntent = new Intent(StartRunActivity.this, RunningInfoActivity.class); 
+				
+				StartRunActivity.this.startActivity(runningInfoIntent);
+				
+			}
+		});
 	}
 
 	@Override
