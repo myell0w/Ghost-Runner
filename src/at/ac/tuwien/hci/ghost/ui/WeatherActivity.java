b3cfc29ef1a752517ef16fed718fccb9a478b2a7
Matthias <matthias.tretter@gmail.com>
2010-04-19 11:53:56 +0200

    WeatherActivity

---
+++ src/at/ac/tuwien/hci/ghost/ui/WeatherActivity.java
@@ -0,0 +1,14 @@
+package at.ac.tuwien.hci.ghost.ui;
+
+import android.app.Activity;
+import android.os.Bundle;
+import at.ac.tuwien.hci.ghost.R;
+
+public class WeatherActivity extends Activity {
+	@Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        this.setContentView(R.layout.weather);
+    }
+}
