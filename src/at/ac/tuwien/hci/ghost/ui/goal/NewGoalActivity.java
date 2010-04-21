e614d22f21963a89bcff3cd032c25c3ba569c22e
Philip Messlehner <philip@PhM-Media.(none)>
2010-04-21 19:54:56 +0200

    SeekBar (Runs not working)

--- src/at/ac/tuwien/hci/ghost/ui/goal/NewGoalActivity.java
+++ src/at/ac/tuwien/hci/ghost/ui/goal/NewGoalActivity.java
@@ -12,8 +12,10 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.SeekBar;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.SeekBar.OnSeekBarChangeListener;
 import at.ac.tuwien.hci.ghost.R;
 import at.ac.tuwien.hci.ghost.data.dao.GoalDAO;
 import at.ac.tuwien.hci.ghost.data.entities.Goal;
@@ -30,6 +32,17 @@ public class NewGoalActivity extends Activity implements TextWatcher {
 	/** menu constants */
 	private final int MENU_GOALEDIT_DISCARD = 101;
 	private final int MENU_GOALEDIT_DELETE = 102;
+	
+	private final int SEEKBAR_WIDTH = 100;
+	
+	private final int MIN_CALORIES = 500;
+	private final int MAX_CALORIES = 50000;
+	
+	private final int MIN_DISTANCE = 10;
+	private final int MAX_DISTANCE = 1000;
+
+	private final int MIN_RUNS = 1;
+	private final int MAX_RUNS = 50;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -50,9 +63,57 @@ public class NewGoalActivity extends Activity implements TextWatcher {
 		ArrayAdapter<Type> adapterType = new ArrayAdapter<Type>(this, android.R.layout.simple_spinner_item, Goal.Type.values());
 		adapterType.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 		spinnerType.setAdapter(adapterType);
+		
+		SeekBar seekGoal = (SeekBar) findViewById(R.id.seekGoal);
+		seekGoal.setMax(SEEKBAR_WIDTH);
+		seekGoal.setOnSeekBarChangeListener(new OnSeekBarChangeListener() {
+
+			@Override
+			public void onProgressChanged(SeekBar seekBar, int progress,
+					boolean fromUser) {
+				int span=1;
+				float value = 0;
+				
+				TextView textGoalValue = (TextView) findViewById(R.id.textGoalValue);
+				
+				if(goal.getType() == Goal.Type.CALORIES) {
+					span = MAX_CALORIES - MIN_CALORIES;
+					value = span/SEEKBAR_WIDTH*progress + MIN_CALORIES;
+					textGoalValue.setText(String.format("%s: %.0f %s",getResources().getString(R.string.goals_calories), value, getResources().getString(R.string.app_unitCalories)));
+				}
+				if(goal.getType() == Goal.Type.DISTANCE) {
+					span = MAX_DISTANCE - MIN_DISTANCE;
+					value = span/SEEKBAR_WIDTH*progress + MIN_DISTANCE;
+					textGoalValue.setText(String.format("%s: %.0f %s",getResources().getString(R.string.goals_distance), value, getResources().getString(R.string.app_unitDistance)));
+				}
+				if(goal.getType() == Goal.Type.RUNS){
+					span = MAX_RUNS - MIN_RUNS;
+					value = span/SEEKBAR_WIDTH*progress + MIN_RUNS;
+					textGoalValue.setText(String.format("%s: %.0f",getResources().getString(R.string.goals_runs), value));
+				}
+				
+				goal.setGoalValue(value);
+				
+			}
+
+			@Override
+			public void onStartTrackingTouch(SeekBar seekBar) {
+				// TODO Auto-generated method stub				
+			}
+
+			@Override
+			public void onStopTrackingTouch(SeekBar seekBar) {
+				// TODO Auto-generated method stub	
+			}
+		});
+		
 		spinnerType.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
 			public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-				saveGoal();
+				Spinner spinnerType = (Spinner) findViewById(R.id.selectedType);
+				goal.setType((Type) spinnerType.getSelectedItem());
+				
+				SeekBar seekGoal = (SeekBar) findViewById(R.id.seekGoal);
+				seekGoal.setProgress((int) SEEKBAR_WIDTH/2);
 			}
 
 			public void onNothingSelected(AdapterView<?> parent) {
@@ -65,9 +126,29 @@ public class NewGoalActivity extends Activity implements TextWatcher {
 			spinnerType.setSelection(goal.getType().ordinal());
 			EditText textGoal = (EditText) findViewById(R.id.textGoal);
 			textGoal.setText(String.valueOf(goal.getGoalValue()));
-
+			
+			//setSeekBar
+			int span=1;
+			float value = 0;
+			
+			if(goal.getType() == Goal.Type.CALORIES) {
+				span = MAX_CALORIES - MIN_CALORIES;
+				value = goal.getGoalValue() - MIN_CALORIES;
+			}
+			if(goal.getType() == Goal.Type.DISTANCE) {
+				span = MAX_DISTANCE - MIN_DISTANCE;
+				value = goal.getGoalValue() - MIN_DISTANCE;
+			}
+			if(goal.getType() == Goal.Type.RUNS){
+				span = MAX_RUNS - MIN_RUNS;
+				value = goal.getGoalValue() - MIN_RUNS;
+			}
+			
+			seekGoal.setProgress((int)value/span*SEEKBAR_WIDTH);
+			
 		} else {
 			header.setText("New Goal");
+			seekGoal.setProgress((int) SEEKBAR_WIDTH/2);
 		}
 
 		/*
