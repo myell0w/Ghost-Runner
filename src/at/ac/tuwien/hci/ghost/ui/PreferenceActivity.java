e54e4391b8343b5ac9ac2cd73c58ff7288ade915
Gerhard Schraml <gerhard@mac-gspot.local>
2010-04-21 15:43:39 +0200

    Settings Screen

---
+++ src/at/ac/tuwien/hci/ghost/ui/PreferenceActivity.java
@@ -0,0 +1,13 @@
+package at.ac.tuwien.hci.ghost.ui;
+
+import android.os.Bundle;
+import at.ac.tuwien.hci.ghost.R;
+
+public class PreferenceActivity extends android.preference.PreferenceActivity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		addPreferencesFromResource(R.xml.preferences);
+		
+	}
+}
