133dcd6ef9345e6f9285d05a4cda72378bdba8e1
Gerhard Schraml <gerhard@mac-gspot.local>
2010-05-06 20:07:38 +0200

    added test framework... tests tbd

---
+++ test/at/ac/tuwien/hci/ghost/AllTests.java
@@ -0,0 +1,16 @@
+package at.ac.tuwien.hci.ghost;
+
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+import android.test.suitebuilder.TestSuiteBuilder;
+
+
+/**
+ * A test suite containing all tests for my application.
+ */
+public class AllTests extends TestSuite {
+    public static Test suite() {
+        return new TestSuiteBuilder(AllTests.class).includeAllPackagesUnderHere().build();
+    }
+}
---
+++ test/at/ac/tuwien/hci/ghost/RunDAOTest.java
@@ -0,0 +1,17 @@
+package at.ac.tuwien.hci.ghost;
+
+
+import junit.framework.Assert;
+import android.test.AndroidTestCase;
+
+
+public class RunDAOTest extends AndroidTestCase {
+
+    public void testSomething() throws Throwable {
+       Assert.assertTrue(1 + 1 == 2);
+    }
+
+    public void testSomethingElse() throws Throwable {
+       Assert.assertTrue(1 + 1 == 3);
+    }
+}
